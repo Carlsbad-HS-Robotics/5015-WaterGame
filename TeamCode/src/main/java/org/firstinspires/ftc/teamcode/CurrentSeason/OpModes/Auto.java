31753a2cbe378d84e0c0b2d6ea9761182efd2a6f
BuffaloWings-5015 <57570696+BuffaloWings-5015@users.noreply.github.com>
2024-03-14 17:12:18 -0700

    cuttle? + idk

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CurrentSeason/OpModes/Auto.java
@@ -0,0 +1,85 @@
+package org.firstinspires.ftc.teamcode.CurrentSeason.OpModes;
+
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.linearOpMode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.roboctopi.cuttlefish.components.Motor;
+import com.roboctopi.cuttlefish.controller.PTPController;
+import com.roboctopi.cuttlefish.controller.Waypoint;
+import com.roboctopi.cuttlefish.localizer.ThreeEncoderLocalizer;
+import com.roboctopi.cuttlefish.queue.PointTask;
+import com.roboctopi.cuttlefish.utils.Direction;
+import com.roboctopi.cuttlefish.utils.Pose;
+import com.roboctopi.cuttlefishftcbridge.devices.CuttleEncoder;
+import com.roboctopi.cuttlefishftcbridge.devices.CuttleRevHub;
+import com.roboctopi.cuttlefishftcbridge.devices.CuttleMotor;
+import com.roboctopi.cuttlefish.controller.MecanumController;
+import com.roboctopi.cuttlefishftcbridge.opmodeTypes.GamepadOpMode;
+
+public class Auto extends GamepadOpMode {
+    CuttleRevHub ctrlHub = new CuttleRevHub(hardwareMap,CuttleRevHub.HubTypes.CONTROL_HUB);
+    CuttleRevHub expHub = new CuttleRevHub(hardwareMap,CuttleRevHub.HubTypes.EXPANSION_HUB);
+    CuttleEncoder leftEncoder ;
+    CuttleEncoder sideEncoder ;
+    CuttleEncoder rightEncoder;
+
+    ThreeEncoderLocalizer encoderLocalizer;
+    public CuttleMotor leftFrontMotor;
+    public CuttleMotor rightFrontMotor;
+    public CuttleMotor rightBackMotor ;
+    public CuttleMotor leftBackMotor  ;
+    MecanumController quinton;
+    PTPController ptpController;
+
+    @Override
+    public void onInit()
+    {
+        leftFrontMotor  = ctrlHub.getMotor(3);
+        rightFrontMotor = ctrlHub.getMotor(2);
+        rightBackMotor  = expHub .getMotor(2);
+        leftBackMotor   = expHub .getMotor(3);
+
+        leftBackMotor .setDirection(Direction.REVERSE);
+        leftFrontMotor.setDirection(Direction.REVERSE);
+
+        leftEncoder  = expHub .getEncoder(3,720*4);
+        sideEncoder  = ctrlHub.getEncoder(0,720*4);
+        rightEncoder = ctrlHub.getEncoder(3,720*4);
+
+        leftEncoder.setDirection(Direction.REVERSE);
+
+        encoderLocalizer = new ThreeEncoderLocalizer(
+                leftEncoder  ,
+                sideEncoder  ,
+                rightEncoder ,
+                29, // Radius of the wheel in mm
+                130.5, // Distance between the two forward facing wheels in mm
+                1.0 //Calibration constant (see below)
+        );
+
+        quinton = new MecanumController(rightFrontMotor,rightBackMotor,leftFrontMotor,leftBackMotor);
+        ptpController = new PTPController(quinton, encoderLocalizer);
+    }
+
+    @Override
+    public void main() {
+
+    }
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+    @Override
+    public void mainLoop()
+    {
+        encoderLocalizer.update();
+        System.out.println(encoderLocalizer.getPos());
+        telemetry.addData("Localizer X:",encoderLocalizer.getPos().getX());
+        telemetry.addData("Localizer Y:",encoderLocalizer.getPos().getY());
+        telemetry.addData("Localizer R:",encoderLocalizer.getPos().getR());
+        telemetry.update();
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CurrentSeason/OpModes/TeleOp.java
@@ -0,0 +1,108 @@
+/* Copyright (c) 2021 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode.CurrentSeason.OpModes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name="TeleOp bomb", group="Meet One")
+public class TeleOp extends LinearOpMode {
+
+    // Declare OpMode members for each of the 4 motors.
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor lfMotor = null;
+    private DcMotor lrMotor = null;
+    private DcMotor rfMotor = null;
+    private DcMotor rrMotor = null;
+
+    @Override
+    public void runOpMode() {
+
+        lfMotor = hardwareMap.get(DcMotor.class, "leftFront");
+        lrMotor = hardwareMap.get(DcMotor.class, "leftRear");
+        rfMotor = hardwareMap.get(DcMotor.class, "rightFront");
+        rrMotor = hardwareMap.get(DcMotor.class, "rightRear");
+
+        lfMotor.setDirection(DcMotor.Direction.REVERSE);
+        lrMotor.setDirection(DcMotor.Direction.REVERSE);
+        rfMotor.setDirection(DcMotor.Direction.REVERSE);
+        rrMotor.setDirection(DcMotor.Direction.FORWARD);
+
+        lfMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        lrMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rfMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rrMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+        runtime.reset();
+
+        while (opModeIsActive()) {
+            double max;
+
+            double axial   = -gamepad1.left_stick_y;
+            double lateral =  gamepad1.left_stick_x;
+            double yaw     =  gamepad1.right_stick_x;
+
+            double leftFrontPower  = axial + lateral + yaw;
+            double rightFrontPower = axial - lateral - yaw;
+            double leftBackPower   = axial - lateral + yaw;
+            double rightBackPower  = axial + lateral - yaw;
+
+            max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
+            max = Math.max(max, Math.abs(leftBackPower));
+            max = Math.max(max, Math.abs(rightBackPower));
+
+            if (max > 1.0) {
+                leftFrontPower  /= max;
+                rightFrontPower /= max;
+                leftBackPower   /= max;
+                rightBackPower  /= max;
+            }
+
+/*
+            leftFrontPower  = gamepad1.x ? 1.0 : 0.0;  // X gamepad
+            leftBackPower   = gamepad1.a ? 1.0 : 0.0;  // A gamepad
+            rightFrontPower = gamepad1.y ? 1.0 : 0.0;  // Y gamepad
+            rightBackPower  = gamepad1.b ? 1.0 : 0.0;  // B gamepad
+*/
+            lfMotor.setPower(leftFrontPower);
+            rfMotor.setPower(rightFrontPower);
+            lrMotor.setPower(leftBackPower);
+            rrMotor.setPower(rightBackPower);
+
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.update();
+        }
+    }}
